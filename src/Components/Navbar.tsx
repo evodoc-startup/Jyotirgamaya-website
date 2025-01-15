2a6feea5a29b3322df781be01207eb4e1716e5c1
saksham <sakshamjain0464@gmail.com>
2025-01-15 23:17:05 +0530

    Fixed Logo issues

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -65,7 +65,7 @@ function Navbar() {
     <nav className={`bg-white py-2 flex justify-center sticky top-0 z-50 w-screen`}>
       <div className="flex md:justify-around justify-between px-6 w-full items-center cursor-pointer text-nowrap">
         <Link href={"/"}>
-          <Image src="logo.png" height={200} width={200} alt="Logo" className="h-20 w-auto aspect-auto" />
+          <Image src="/Logo.png" height={200} width={200} alt="Logo" className="h-20 w-auto aspect-auto" />
         </Link>
         <ul className="sm:flex hidden w-fit md:space-x-10 space-x-4 md:justify-normal justify-end text-base">
           <NavItems items={navItems} phoneNavOpen = {phoneNavOpen} setPhoneNavOpen={setPhoneNavOpen}/>
